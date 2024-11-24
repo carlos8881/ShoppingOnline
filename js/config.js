fd851741e283241af68e467b01ab5fee915eb0ff
carlos8881 <yoyo19990801@gmail.com>
2024-11-24 08:58:17 +0800

    v8.1

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.AppConfig = {
-    API_URL: 'https://d1khcxe0f8g5xw.cloudfront.net/' // 'http://localhost:3000' or 'https://d1khcxe0f8g5xw.cloudfront.net/'
+    API_URL: 'https://d1khcxe0f8g5xw.cloudfront.net' // 'http://localhost:3000' or 'https://d1khcxe0f8g5xw.cloudfront.net'
 };
