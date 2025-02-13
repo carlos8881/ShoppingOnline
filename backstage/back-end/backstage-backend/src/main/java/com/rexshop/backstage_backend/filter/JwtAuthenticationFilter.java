24ffe20a33be3a947d33bbc218a0905ca6f54f7f
carlos8881 <yoyo19990801@gmail.com>
2025-02-13 23:02:44 +0800

    v9.5.1

--- backstage/back-end/backstage-backend/src/main/java/com/rexshop/backstage_backend/filter/JwtAuthenticationFilter.java
+++ backstage/back-end/backstage-backend/src/main/java/com/rexshop/backstage_backend/filter/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -28,7 +29,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain chain)
             throws ServletException, IOException {
 
         // 從 Header 或 Cookie 取得 JWT
